111187a7b7f2e8abd19498b7df8bcb0b6faa59b7
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-12-16 12:22:39 -0800

    Create navbar and login-register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginRegisterComponent,
+  },
+  {
+    path: 'register',
+    component: LoginRegisterComponent,
+  },
+  // TODO - user profile route
+  // TODO - reset password route? or include with user profile
+  // TODO - view claims route
+  // TODO - approve/deny claims route
+  // TODO - change patient/employee roles route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginRegisterComponent
   ],
   imports: [
     BrowserModule,
