0008dbd2e4cf4ccc30a563ec677fd4fae0b683ba
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-12-19 11:59:07 -0800

    Generate login-register model

---
+++ src/app/models/login-register.spec.ts
@@ -0,0 +1,7 @@
+import { LoginRegister } from './login-register';
+
+describe('LoginRegister', () => {
+  it('should create an instance', () => {
+    expect(new LoginRegister()).toBeTruthy();
+  });
+});
